48dd59f6289c101814cbe474390bd060032ec450
Claudio Raul Brito Mercedes <clbrito@apap.com.do>
2022-10-02 21:15:35 -0400

    chore: adding external services

---
+++ mono-financialbot-backend-cs-external-serivces/Providers/RabbitMQ/Interfaces/IRabbitMQReciever.cs
@@ -0,0 +1,7 @@
+﻿namespace mono_financialbot_backend_cs_external_serivces.Providers.RabbitMQ.Interfaces
+{
+    public interface IRabbitMQReciever
+    {
+        void InitializeListeners();
+    }
+}
